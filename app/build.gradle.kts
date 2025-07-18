9961660fc7beaa4c353fccf36826efc6a77455f3
Squti <sqquti@gmail.com>
2025-07-18 14:52:59 +0200

    Fix Failed to read key exception

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
 
     signingConfigs {
         create("release") {
-            storeFile = file(System.getenv("SIGNING_STORE_FILE") ?: "release.keystore")
+            storeFile = file(System.getenv("SIGNING_STORE_FILE") ?: "release.jks")
             storePassword = System.getenv("SIGNING_STORE_PASSWORD")
             keyAlias = System.getenv("SIGNING_KEY_ALIAS")
             keyPassword = System.getenv("SIGNING_KEY_PASSWORD")
